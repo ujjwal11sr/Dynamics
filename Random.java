64cb97f64e4fe0f33fa9e4dfbe86df93056ff4f3
Satnam412 <37145383+Satnam412@users.noreply.github.com>
2020-10-25 23:26:49 +0530

    input from user added

--- Random.java
+++ Random.java
@@ -4,9 +4,10 @@ import java.util.*;
 public class Random{
 	
 	public static void main(String args[]){
-		System.out.println(Math.random());
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
 		
-		for(int i=0;i<5;i++){
+		for(int i=0;i<n;i++){
 			System.out.print((int)(Math.random()*(i+1)));
 		}
 	}
